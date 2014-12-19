3b342e2d47731fc3cafc3368ad8920fe2f732958
Lex.Lin <lexjoy@126.com>
2014-12-19 17:08:13 +0800

    Delete LogHandler.java

--- src/me/lexjoy/utils/LogHandler.java
+++
@@ -1,9 +0,0 @@
-package me.lexjoy.utils.log;
-
-public interface LogHandler {
-  void v(String tag, String message);
-  void i(String tag, String message);
-  void w(String tag, String message);
-  void d(String tag, String message);
-  void e(String tag, String message);
-}
