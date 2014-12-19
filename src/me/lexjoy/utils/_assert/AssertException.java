860a439f073e178ebf5b167d1bebda1eacd10f11
Lex.Lin <lexjoy@126.com>
2014-12-19 17:05:53 +0800

    Create AssertException.java

---
+++ src/me/lexjoy/utils/_assert/AssertException.java
@@ -0,0 +1,14 @@
+package me.lexjoy.utils._assert;
+
+public class AssertException extends RuntimeException {
+
+  public AssertException() {
+  }
+
+  public AssertException(String message) {
+    super(message);
+  }
+
+  private static final long serialVersionUID = 1L;
+
+}
